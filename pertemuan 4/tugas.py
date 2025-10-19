d06c9ed486525bebbe04f24d365ddb3af2a0e47a
zayennn <zaayeennn@gmail.com>
2025-10-19 21:22:37 +0700

    update

--- pertemuan 4/tugas.py
+++ pertemuan 4/tugas.py
@@ -1,39 +1,46 @@
-# =================== Studi Kasus ===================
-pembeli=input("Input Nama Pembeli : ")
-no_hp=input("Input No. Handphone : ")
-jurusan=input("Input Jurusan [SBY/BL/LMP] : ")
+# Program menghitung total gaji karyawan PT. Dingin Damai
+gaji_pokok = 300000
 
-if jurusan=="SBY":
-	namajurusan="Surabaya"
-	harga=300000
-elif jurusan=="BL":
-	namajurusan="Bali"
-	harga=350000
+golongan = int(input("Masukkan golongan (1/2/3): "))
+pendidikan = input("Masukkan pendidikan (SMA/D1/D3/S1): ").upper()
+jam_kerja = int(input("Masukkan jumlah jam kerja: "))
+
+# Hitung tunjangan jabatan
+if golongan == 1:
+    tunjangan_jabatan = 0.05 * gaji_pokok
+elif golongan == 2:
+    tunjangan_jabatan = 0.10 * gaji_pokok
+elif golongan == 3:
+    tunjangan_jabatan = 0.15 * gaji_pokok
 else:
-	namajurusan="Lampung"
-	harga=500000
-
-jumlah=int(input("Masukkan Jumlah Beli : "))
-
-if jumlah>=3 :
-	potongan=(jumlah*harga)*0.1
+    tunjangan_jabatan = 0
+    
+# Hitung tunjangan pendidikan
+if pendidikan == "SMA":
+    tunjangan_pendidikan = 0.025 * gaji_pokok
+elif pendidikan == "D1":
+    tunjangan_pendidikan = 0.05 * gaji_pokok
+elif pendidikan == "D3":
+    tunjangan_pendidikan = 0.20 * gaji_pokok
+elif pendidikan == "S1":
+    tunjangan_pendidikan = 0.30 * gaji_pokok
 else:
-	potongan=0
-total=(jumlah*harga)-potongan
+    tunjangan_pendidikan = 0
+    
+# Hitung honor lembur
+if jam_kerja > 8:
+    lembur = (jam_kerja - 8) * 3500
+else:
+    lembur = 0
 
-print("------------------------------------")
-print(" PENJUALAN TIKET BUS")
-print(" XYZ")
-print("------------------------------------")
-print("Nama Pembeli : "+str(pembeli))
-print("No. Handphone : "+str(no_hp))
-print("Kode Jurusan yang dipilih : "+str(jurusan))
-print("Nama Kota Tujuan : "+str(namajurusan))
-print("Harga : ",+(harga))
-print("Jumlah Beli : ",+(jumlah))
-print("------------------------------------")
-print("potongan yang didapat : ",+(potongan))
-print("Total Bayar : ",+(total))
-ubay=int(input("Masukkan Uang Bayar : "))
-uangkembali=ubay-total
-print("Uang Kembali : ",+uangkembali)
+# Hitung total gaji
+total_gaji = gaji_pokok + tunjangan_jabatan + tunjangan_pendidikan + lembur
+
+# Tampilkan hasil
+print("\n=== RINCIAN GAJI KARYAWAN ===")
+print(f"Gaji Pokok           : Rp {gaji_pokok}")
+print(f"Tunjangan Jabatan    : Rp {tunjangan_jabatan}")
+print(f"Tunjangan Pendidikan : Rp {tunjangan_pendidikan}")
+print(f"Honor Lembur         : Rp {lembur}")
+print("--------------------------------")
+print(f"Total Gaji           : Rp {total_gaji}")
