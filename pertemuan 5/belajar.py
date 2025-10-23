b6d38a2689b433c51569d230152fe45793ebd092
zayennn <zaayeennn@gmail.com>
2025-10-23 15:35:39 +0700

    update

--- pertemuan 5/belajar.py
+++ pertemuan 5/belajar.py
@@ -2,3 +2,10 @@ mapel = [ 'matematika', 'fisika', 'kimia']
 # iterasi list menggunakan indeks
 for i in range(len(mapel)):
     print("Saya suka", mapel[i])
+    
+    
+    
+# ======================================
+
+
+
