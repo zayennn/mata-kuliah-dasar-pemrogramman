c13a0c208d0915f6e8d8a3dd2d2cbebeea2932d4
zayennn <zaayeennn@gmail.com>
2025-10-23 16:11:45 +0700

    update

--- pertemuan 5/belajar.py
+++ pertemuan 5/belajar.py
@@ -24,3 +24,17 @@ text = "PemrogrammanPython"
 
 for tulisan in text :
     print(tulisan)
+    
+    
+    
+# ======================================
+
+ulang = int(input("Masukkan jumlah data yang ingin diinput: "))
+
+for i in range(ulang):
+    print("Data Ke - " + str(i + 1))
+    nama = input("Masukkan Nim anda : ")
+    uts = int(input("Masukkan Nilai UTS anda : "))
+    uas = int(input("Masukkan Nilai UAS anda : "))
+    print("NIM anda adalah %s nilai UTS anda %i nilai UAS anda %i" % (nama, uts, uas))
+    print("----------------------------------------\n")
