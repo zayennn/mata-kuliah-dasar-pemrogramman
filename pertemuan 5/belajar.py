0f7392c9190fcc7ea7b221c958b2363eebc5b1fb
zayennn <zaayeennn@gmail.com>
2025-10-23 15:38:32 +0700

    update

--- pertemuan 5/belajar.py
+++ pertemuan 5/belajar.py
@@ -8,10 +8,9 @@ for i in range(len(mapel)):
 # ======================================
 
 
-count = 0
-
+count = 1
 
 while ( count < 5 ) :
-    print('count is : ', count)
+    print('count is : ', count + 1)
     count += 1
 print('good bye!')
