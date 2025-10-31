3dccae7779d49e68a9f4e5457c2229aa08812ad9
zayennn <zaayeennn@gmail.com>
2025-10-31 15:27:49 +0700

    update

--- pertemuan 6/tugas.py
+++ pertemuan 6/tugas.py
@@ -7,3 +7,8 @@ ulang = 2
 
 for i in range(ulang):
     print("data ke - " + i + 1)
+    list_nim.append(int(input('masukan nim anda : ')))
+    list_uts.append(int(input('masukan nilai uts anda : ')))
+    list_uas.append(int(input('masukan nilai uas anda : ')))
+    
+    
