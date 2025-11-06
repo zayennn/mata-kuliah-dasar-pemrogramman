bd3975adb028fe4e98ba50c1a86d4691fb8541e0
zayennn <zaayeennn@gmail.com>
2025-11-06 13:57:12 +0700

    update

---
+++ pertemuan 6/tugas2.py
@@ -0,0 +1,16 @@
+mat = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(f"""
+- baris pertama: kolom pertama : {mat[0][0]}
+- baris pertama kolom kedua : {mat[0][1]}
+- baris pertama kolom ketiga : {mat[0][2]}
+
+- baris ketiga kolom ketiga : {mat[2][2]}
+
+- baris keempat kolom pertama : {mat[3][0]}
+""")
